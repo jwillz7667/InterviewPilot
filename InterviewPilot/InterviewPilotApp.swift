0232aa7a5eadc01c893eb8e8581bc73c6dcaf36b
jwillz7667 <jwillz7667@gmail.com>
2026-03-09 22:02:10 -0500

    Initial Commit

---
+++ InterviewPilot/InterviewPilotApp.swift
@@ -0,0 +1,32 @@
+//
+//  InterviewPilotApp.swift
+//  InterviewPilot
+//
+//  Created by Justin Williams on 3/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InterviewPilotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
